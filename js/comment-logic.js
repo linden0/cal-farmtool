ee25204e7f8c59dcad54951d9cc8e07859a08dee
linden <linden.wang04@gmail.com>
2022-08-11 09:33:22 -0700

    First Commit

---
+++ js/comment-logic.js
@@ -0,0 +1,44 @@
+var config = {
+    apiKey: "AIzaSyAO2Vlrbnmr3-d8pryaDjSgyYgPGlJC6us",
+    authDomain: "droughttool.firebaseapp.com",
+    databaseURL: "https://droughttool-default-rtdb.firebaseio.com",
+    projectId: "droughttool",
+    storageBucket: "droughttool.appspot.com",
+    messagingSenderId: "938533200890"
+};
+
+firebase.initializeApp(config);
+const rootRef = firebase.database().ref();
+const commentsRef = rootRef.child('comments');
+document.getElementById("btnSubmitComment").addEventListener("click", function () {
+    //Replace line breaks in comment with br tags.
+    var newcomment = document.getElementById('txComment').value.replace(/\n/g, "<br>");
+    //Define a new, keyed post.
+    var newPostRef = commentsRef.push();
+    //Fill tne new keyed post with data
+    newPostRef.set({
+        name: document.getElementById('tbName').value.trim(),
+        comment: newcomment.trim(),
+        frompage: location.pathname,
+        when: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+function showpastcomments() {
+    var showat = document.getElementById('pastcomments');
+    //Get comments whose from page equals this page's pathname.
+    var commentsRef = firebase.database().ref('comments/').orderByChild('frompage').equalTo(location.pathname);
+    commentsRef.once('value', function (snapshot) {
+        snapshot.forEach(function (itemSnapshot) {
+            //Get the object for one snapshot
+            var itemData = itemSnapshot.val();
+            var comment = itemData.comment;
+            var name = itemData.name;
+            var when = new Date(itemData.when).toLocaleDateString("en-us");
+            showat.innerHTML += "<li>" + comment + "<span> -- " + name + " (" + when +
+                ")</span></li>";
+        })
+    })
+}
+//Called when page first opens and also after Submit button click to show all comments for this page.
+showpastcomments()
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require("cheerio");
+const cors = require('cors');
+
+const app = express();
+
+const url = 'https://cdec.water.ca.gov/reportapp/javareports?name=STORAGEW';
+
+app.listen(8080, function() {
+    console.log('listening on port 8080');
+})
+
+app.get('/', (req, res) => {
+    res.send('CDEC Water Resevoir API. This API scrapes the latest California resevoir data found at https://cdec.water.ca.gov/reportapp/javareports?name=STORAGEW, and returns it as an object of 2d arrays, ready to load to Google Charts.');
+})
+
+app.get('/api', async (req, res) => {
+    const resevoirToDataTable = {};
+
+    const response = await axios('https://cdec.water.ca.gov/reportapp/javareports?name=STORAGEW');
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const allRows = $('table.data > tbody:nth-child(1) > tr');
+
+    const numCols = $('table.data > tbody:nth-child(1) > tr:nth-child(2) > th').length;
+    const startCol = numCols - 6;
+
+    const [numRows, startRow] = [12, 3];
+
+    //push year headers (2017, 2018, etc.) to template
+    let template = [['Year', 'Water Storage (1000 acre feet)']];
+    $('table.data > tbody:nth-child(1) > tr:nth-child(2) > th').each((index, element) => {
+        if (index >= startCol && index < numCols)
+            template.push([$(element).text(), '']);
+    });
+
+    allRows.each((index, element) => {
+        if (index >=startRow && index < numRows) {
+
+            const tds = $(element).find('td');
+            const region = $(tds[0]).text();
+
+            let dataTableRow = 1;
+            value = JSON.parse(JSON.stringify(template)); //deepcopy of template
+            //push yearly water data to copy
+            for (let col=startCol; col<numCols; col ++) {
+                let acreFeet = $(tds[col]).text()
+                value[dataTableRow][1] = acreFeet
+                dataTableRow += 1
+            }
+
+            resevoirToDataTable[region] = value
+        }
+    })
+    
+    res.json(resevoirToDataTable);
+        
+})

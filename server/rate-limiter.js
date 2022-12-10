c01629b3b6a3cc586c4a69e0edaddf2fbab2d1d6
linden0 <linden.wang04@gmail.com>
2022-12-10 14:40:22 -0800

    Refactoring and included deploy details

--- server/rate-limiter.js
+++ server/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimiter = require("express-rate-limit");
  
 const limiter = rateLimiter({
-    max: 5,
+    max: 5, //max requests per time frame
     windowMS: 10000, // 10 seconds
     message: "You can't make any more requests at the moment. Try again later",
 });
--- server/server.js
+++ server/server.js
@@ -6,14 +6,11 @@ const limiter = require('./rate-limiter');
 app.use(limiter);
 const routes = require("./routes");
 app.use(routes);
-
-const PORT = process.env.PORT || 5000;
-
-  
 app.use(cors({
     origin: '*'
 }));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 });
